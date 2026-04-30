c269f09138b83c28476243ab428108a2595233ca
bohdan0089 <bodiks18@gmail.com>
2026-04-30 19:30:10 +0200

    feat: add user login, token and trip schemas

---
+++ schemas/schemas_trip.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, ConfigDict
+from core.enum import Status
+from datetime import datetime
+
+class TripCreate(BaseModel):
+    pickup_address: str
+    dropoff_address: str
+
+
+class ResponseTrip(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    passenger_id: int
+    driver_id: int | None
+    status: Status
+    pickup_address: str
+    dropoff_address: str
+    created_at: datetime
--- schemas/schemas_user.py
+++ schemas/schemas_user.py
@@ -1,21 +1,28 @@
 from pydantic import BaseModel, EmailStr, ConfigDict
 from core.enum import Role
+from datetime import datetime
 
 
+
+
+class UserLogin(BaseModel):
+    password: str
+    email: EmailStr
 
 class RegisterUser(BaseModel):
     full_name: str
     email: EmailStr
     password: str
     
-
-    
 class ResponseUser(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
+    email: EmailStr
     full_name: str
     email: EmailStr
     role: Role
+    is_active: bool
+    created_at: datetime
 
 
